4e7ea98fbd4a5a31e508129983ec66d80833f41a
fernando919191 <fer982756@gmail.com>
2025-08-25 21:00:01 -0600

    Create tokens.py

---
+++ funcionamiento/tokens.py
@@ -0,0 +1,6 @@
+# funcionamiento/tokens.py
+
+TOKENS = {
+    "BOT_1": "8263797050:AAHGas43emDjn6OdywEx5R3v_Y-wlxDh91I",  # Reemplaza con el token real
+    # "BOT_2": "OTRO_TOKEN"  # Si luego necesitas más
+}
