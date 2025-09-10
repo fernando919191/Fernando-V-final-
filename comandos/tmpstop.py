55ff9dbfce1c11d3c1078b471a8519fc58d43db9
fernando919191 <fer982756@gmail.com>
2025-09-09 19:46:36 -0600

    Actualizar tmpstop.py

--- comandos/tmpstop.py
+++ comandos/tmpstop.py
@@ -1,6 +1,1 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-from comandos.tmp import tmp_stop
-
-async def tmpstop(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await tmp_stop(update, context)
+# fail
