10db1fcca09e9300c58e1ebc7e5f73ba0c2fe100
fernando919191 <fer982756@gmail.com>
2025-09-09 19:46:59 -0600

    Actualizar tmpstatus.py

--- comandos/tmpstatus.py
+++ comandos/tmpstatus.py
@@ -1,6 +1,1 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-from comandos.tmp import tmp_status
-
-async def tmpstatus(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await tmp_status(update, context)
+# fail tmb we (implemente en un solo código)
